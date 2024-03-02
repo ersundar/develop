7d0840951054430678f343ffc55c2005200165f9
ersundar <ersundar1996@gmail.com>
2024-03-02 16:39:51 +0530

    Add Global sql

---
+++ GTT.sql
@@ -0,0 +1,17 @@
+
+
+-- Create a Global Temporary Table in Oracle
+
+CREATE GLOBAL TEMPORARY TABLE TempEmployees (
+    EmployeeID NUMBER,
+    FirstName VARCHAR2(50),
+    LastName VARCHAR2(50),
+    Department VARCHAR2(50)
+) ON COMMIT DELETE ROWS;
+
+CREATE GLOBAL TEMPORARY TABLE TempEmployees (
+    EmployeeID NUMBER,
+    FirstName VARCHAR2(50),
+    LastName VARCHAR2(50),
+    Department VARCHAR2(50)
+) ON COMMIT PRESERVE ROWS;
