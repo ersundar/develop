2c2662a56a14ac2eb99c030a84cf8204aca7b166
ersundar <ersundar1996@gmail.com>
2024-03-02 16:57:56 +0530

    Add mail

--- GTT.sql
+++ GTT.sql
@@ -5,6 +5,7 @@
 CREATE GLOBAL TEMPORARY TABLE TempEmployees (
     EmployeeID NUMBER,
     salary number,
+    mail varchar,
     LastName VARCHAR2(50),
     Department VARCHAR2(50)
 ) ON COMMIT DELETE ROWS;
