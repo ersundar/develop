947fcab20d3ac18496bf9dab6b44458eb6d5db83
ersundar <ersundar1996@gmail.com>
2024-03-02 16:50:20 +0530

    delete firstname

--- GTT.sql
+++ GTT.sql
@@ -4,7 +4,6 @@
 
 CREATE GLOBAL TEMPORARY TABLE TempEmployees (
     EmployeeID NUMBER,
-    FirstName VARCHAR2(50),
     LastName VARCHAR2(50),
     Department VARCHAR2(50)
 ) ON COMMIT DELETE ROWS;
