030a0cd3de8cc343ca0e5e7890d6f9e0f105f369
ersundar <161921456+ersundar@users.noreply.github.com>
2024-03-02 17:17:08 +0530

    ne

--- GTT.sql
+++ GTT.sql
@@ -1,7 +1,3 @@
-
-
--- Create a Global Temporary Table in Oracle
-
 CREATE GLOBAL TEMPORARY TABLE TempEmployees (
     EmployeeID NUMBER,
     FirstName VARCHAR2(50),
@@ -14,5 +10,3 @@ CREATE GLOBAL TEMPORARY TABLE TempEmployees (
     FirstName VARCHAR2(50),
     LastName VARCHAR2(50),
     Department VARCHAR2(50)
-) ON COMMIT PRESERVE ROWS;
-
